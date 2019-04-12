2138a9e518ee2d0eead732cce7434bc183909d18
Grey Ruessler <greyruessler@gmail.com>
2019-04-11 23:45:19 -0500

    fixed aaron's short arguments; made help work on raw functions and not strings

--- modules/rsa_d.py
+++ modules/rsa_d.py
@@ -3,7 +3,7 @@ def rsa_d(phi_n, e):
 
 registerFunction("rsa_d", {
     "name" : "Find the decryption exponential for RSA",
-    "arguments_short":["phi_n, e"],
+    "arguments_short":["phi_n","e"],
     "arguments":["phi_n you can run rsa_phi for this", "the public RSA encryption exponential e"],
     "description":"Returns the decryption exponential for RSA"
 })
