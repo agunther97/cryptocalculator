d850c681dda12c6b442d7011b485463e324e835a
rocke97 <aarongunther97@gmail.com>
2019-04-11 20:35:48 -0500

    added book sam function

--- modules/sam.py
+++ modules/sam.py
@@ -1,4 +1,13 @@
-def sam(x,y,z):
-    print(x)
-    print(y)
-    print(z)
+import sys
+import math
+
+def sam(x, c, n):
+    c = int(c)
+    c = '{0:b}'.format(c) #to binary
+    z = 1
+    l = len(c)
+    for i in range(l):
+        z = (math.pow(z, 2)) % n
+        if(c[i] == "1"):
+            z = (z*x) % n
+    return z
