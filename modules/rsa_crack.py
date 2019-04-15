83bfc7d5b4adfb5cb1a93223b3a1b9aa05c4a933
Grey Ruessler <greyruessler@gmail.com>
2019-04-15 09:27:59 -0500

    ciphertext is one word lol

--- modules/rsa_crack.py
+++ modules/rsa_crack.py
@@ -1,9 +1,9 @@
-def rsa_crack(cipher_text_numbers, d, n):
-    return [pow(c, d, n) for c in cipher_text_numbers]
+def rsa_crack(ciphertext_numbers, d, n):
+    return [pow(c, d, n) for c in ciphertext_numbers]
 
 registerFunction("rsa_crack", {
     "name" : "Crack RSA encrypted text",
-    "arguments_short":["cipher_text_numbers","d","n"],
+    "arguments_short":["ciphertext_numbers","d","n"],
     "arguments":["cipher text list in numbers", "decryption exponent d", "mod value"],
     "description":"Returns the plain text in numbers of RSA encrypted text"
 })
