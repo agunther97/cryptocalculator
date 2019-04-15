c1b2da2c32ed4698fbadaf4fa6ed7eb969fb5082
Grey Ruessler <greyruessler@gmail.com>
2019-04-15 09:35:19 -0500

    changed rsa_crack to rsa_decrypt and rsa_encrypt

--- modules/rsa_crack.py
+++
@@ -1,9 +0,0 @@
-def rsa_crack(ciphertext_numbers, d, n):
-    return [pow(c, d, n) for c in ciphertext_numbers]
-
-registerFunction("rsa_crack", {
-    "name" : "Crack RSA encrypted text",
-    "arguments_short":["ciphertext_numbers","d","n"],
-    "arguments":["cipher text list in numbers", "decryption exponent d", "mod value"],
-    "description":"Returns the plain text in numbers of RSA encrypted text"
-})
---
+++ modules/rsa_exp.py
@@ -0,0 +1,20 @@
+def rsa_encrypt(plaintext_numbers, e, n):
+    if type(plaintext_numbers) is int:
+        plaintext_numbers = [plaintext_numbers]
+    return [pow(p, e, n) for p in plaintext_numbers]
+def rsa_decrypt(ciphertext_numbers, d, n):
+    return rsa_encrypt(ciphertext_numbers, d, n)
+
+registerFunction("rsa_encrypt", {
+    "name" : "Encrypt data with RSA",
+    "arguments_short":["plaintext_numbers","d","n"],
+    "arguments":["plaintext list in numbers", "encryption exponent e", "mod value"],
+    "description":"Returns the ciphertext in numbers for a given plaintext"
+})
+
+registerFunction("rsa_decrypt", {
+    "name" : "Decrypt RSA encrypted text",
+    "arguments_short":["ciphertext_numbers","d","n"],
+    "arguments":["ciphertext list in numbers", "decryption exponent d", "mod value"],
+    "description":"Returns the plaintext in numbers of RSA encrypted text"
+})
