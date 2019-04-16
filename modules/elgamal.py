0545fd44246346f06ec48235c632b98ed1a96303
Grey Ruessler <greyruessler@gmail.com>
2019-04-15 20:57:46 -0500

    enhanced verbose output

--- modules/elgamal.py
+++ modules/elgamal.py
@@ -1,17 +1,25 @@
-def elgamal_alpha(p):
+def elgamal_alpha(p,verbose=False):
+    if verbose:
+        print("Generating alpha for " + str(p))
     for i in range(1,p):
+        if verbose:
+            print("Checking value "+str(i))
         if is_primitive_root(i,p):
+            if verbose:
+                print("Found primitive: " + str(i))
             return i
     return -1
 
-def elgamal_beta(p,alpha,a):
+def elgamal_beta(p,alpha,a,verbose=False):
+    if verbose:
+        print("beta=sam("+str(alpha)+","+str(a)+","+str(p)+")="+str(pow(alpha,a,p)))
     return pow(alpha,a,p)
 
-def elgamal_encrypt(x,p,alpha,a):
-    return elgamale(x,p,alpha,elgamal_beta(p,alpha,a))
+def elgamal_encrypt(x,p,alpha,a,k=None,verbose=False):
+    return elgamale(x,p,alpha,elgamal_beta(p,alpha,a),k=k,verbose=verbose)
 
-def elgamal_decrypt(y,p,a):
-    return elgamald(y,p,a)
+def elgamal_decrypt(y,p,a,verbose=False):
+    return elgamald(y,p,a,verbose=verbose)
 
 def elgamale(x,p,alpha,beta,k=None,verbose=False):
     lx=x
@@ -33,12 +41,56 @@ def elgamale(x,p,alpha,beta,k=None,verbose=False):
             k=None
     return w
 
-def elgamald(y,p,a):
+def elgamald(y,p,a,verbose=False):
     t=[]
     d=len(y[1])
     for j in range(0,d,1):
         y1=y[0][j]
         y2=y[1][j]
+        if verbose:
+            print("x_"+str(j)+"=mod("+str(y2)+"*inv(sam("+str(y1)+","+str(a)+","+str(p)+"),"+str(p)+"),"+str(p)+")")
         s=mod(y2*inv(pow(y1,a,p),p),p)
         t.append(s)
     return t
+
+registerFunction("elgamal_alpha", {
+    "name" : "Elgamal Generate Alpha",
+    "arguments_short":["p","verbose=False"],
+    "arguments":["modulous","print commands"],
+    "description":"Computes first primitive root of a prime"
+})
+
+registerFunction("elgamal_beta", {
+    "name" : "Elgamal Generate Alpha",
+    "arguments_short":["p","alpha","a","verbose=False"],
+    "arguments":["modulous","alpha","secret","print commands"],
+    "description":"Computes beta for Elgamal given modulo, alpha, and a"
+})
+
+registerFunction("elgamale", {
+    "name" : "Elgamal Encryption",
+    "arguments_short":["data","p","alpha","a","k=None","verbose=False"],
+    "arguments":["plaintext number list","modulous","alpha","beta","random value","print commands"],
+    "description":"Computes ciphertext using Elgamal encryption"
+})
+
+registerFunction("elgamal_encrypt", {
+    "name" : "Elgamal Encryption",
+    "arguments_short":["data","p","alpha","a","k=None","verbose=False"],
+    "arguments":["plaintext number list","modulous","alpha","secret value","random value","print commands"],
+    "description":"Computes ciphertext using Elgamal encryption"
+})
+
+registerFunction("elgamald", {
+    "name" : "Elgamal Decryption",
+    "arguments_short":["data","p","a"],
+    "arguments":["ciphertext number list","modulous","a"],
+    "description":"Computes plaintext using Elgamal decryption"
+})
+
+registerFunction("elgamal_decrypt", {
+    "name" : "Elgamal Decryption",
+    "arguments_short":["data","p","a"],
+    "arguments":["ciphertext number list","modulous","a"],
+    "description":"Computes plaintext using Elgamal decryption"
+})
--- tests/elgamaltest.py
+++ tests/elgamaltest.py
@@ -3,9 +3,9 @@ txt=input("Enter a test string:\n")
 p=55207
 a=69
 k=None
-alpha=elgamal_alpha(p)
+alpha=elgamal_alpha(p,verbose=True)
 
 n=ttn(txt,3)
 y=elgamale(n,p,alpha,elgamal_beta(p,alpha,a),verbose=True,k=k)
-x=elgamald(y,p,a)
+x=elgamald(y,p,a,verbose=True)
 print(ntt(x))
