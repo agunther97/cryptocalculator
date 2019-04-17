f8753d28197430405b5eeff4d42204c7e7ba82e5
rocke97 <aarongunther97@gmail.com>
2019-04-16 19:14:49 -0500

    Added chinese_remainder solver

---
+++ modules/chinese_remainder.py
@@ -0,0 +1,28 @@
+from functools import reduce
+
+def chinese_remainder(number_values, mod_values, verbose=False):
+    M = reduce((lambda x, y: x * y), mod_values)
+    m_subs = list(map((lambda x: M/x), mod_values))
+    y_subs = []
+    for i in range(len(m_subs)):
+        y_subs.append(int(multiplicative_inverse(m_subs[i], mod_values[i])))
+    x = 0
+    x_calc_string = 'Calculating X as '
+    for i in range(len(number_values)):
+        x += number_values[i]*m_subs[i]*y_subs[i]
+        x_calc_string += str(number_values[i]) + ' * ' + str(m_subs[i]) + ' * ' + str(y_subs[i]) + ' + '
+    x = x%M
+    if verbose:
+        print('Calculating M as: ' + '*'.join(map(str, mod_values)) + ' = ' + str(M))
+        print('Calculating M sub 1, ..., M sub N as ' + ','.join(map(str, m_subs)))
+        print ('Calculating Y sub 1, ...., Y sub N as ' + ','.join(map(str, y_subs)))
+        x_calc_string = x_calc_string[:-3]
+        print (x_calc_string + ' = ' + str(int(x)))
+    return int(x)
+
+registerFunction("chinese_remainder", {
+    "name" : "Solves chinese remainder theorem for a set of congruences",
+    "arguments_short":["number_values","mod_values","verbose=False"],
+    "arguments":["The number values of the problem in a list (i.e. for x = 14 mod 29 you'd put 4)", "The mod values of the problem in a list"],
+    "description":"Returns the solution to a set of congruences using chinease remainder theorem"
+})
---
+++ tests/chinesetest.py
@@ -0,0 +1,4 @@
+print('Using number values of 4,16,19 and mod values of 29,30,31')
+print('Expected ans: 9226')
+print('Real ans:')
+print(chinese_remainder([4,16,19],[29,30,31]))
