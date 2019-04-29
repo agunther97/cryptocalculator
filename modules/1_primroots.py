9082a177c5ffd1e37d726df136a2e70149f63dd1
Grey Ruessler <greyruessler@gmail.com>
2019-04-29 16:39:44 -0500

    primitive elems function

--- modules/1_primroots.py
+++ modules/1_primroots.py
@@ -1,5 +1,15 @@
-def primitive_roots(modulo):
+def primitive_roots(modulo,verbose=False):
     coprime_set = {num for num in range(1, modulo) if gcd(num, modulo) == 1}
+    if verbose:
+        print("coprimes:")
+        isList=True
+        for num in range(1,modulo):
+            if gcd(num,modulo)!=1:
+                isList=False
+        if isList:
+            print(str(1)+"->"+str(modulo-1))
+        else:
+            print(coprime_set)
     return [g for g in range(1, modulo) if coprime_set == {pow(g, powers, modulo)
             for powers in range(1, modulo)}]
 
@@ -12,3 +22,6 @@ def is_primitive_root(n,mod):
         else:
             cache[x]=i
     return True
+
+def primitive_elements(mod,verbose=False):
+    return primitive_roots(mod,verbose)
