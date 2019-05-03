f3c9a3f51fef2ede9d6ef3e86f1c7a99bea9cdf2
Grey Ruessler <greyruessler@gmail.com>
2019-05-02 23:54:35 -0500

    fermat factoring algorithm

---
+++ modules/factor_fermat.py
@@ -0,0 +1,15 @@
+from math import ceil, floor, sqrt
+
+def factor(n):
+    t=floor(sqrt(n)) #t is the square root of n, rounded down
+    sq=pow(t,2)-n #calculate s
+    while(t<n):
+        if sqrt(sq)==floor(sqrt(sq)): #if it’s a perfect square
+            break #stop looking
+        else:
+            t+=1 #check next t 
+            sq=pow(t,2)-n #calculate s
+    if sqrt(sq)==floor(sqrt(sq)): #if it’s a perfect square
+        return [t,int(sqrt(sq))]
+
+print(factor(310485170747))
