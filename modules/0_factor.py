7b69c7e742c9ef456f792a1f79c744db5d029d24
Grey Ruessler <greyruessler@gmail.com>
2019-05-03 19:38:23 -0500

    added prime factorization w/ tuples

--- modules/0_factor.py
+++ modules/0_factor.py
@@ -9,7 +9,20 @@ def factor_prime(nr):
             nr = nr / i
         else:
             i = i + 1
-    return factors
+    return factors #(factor,factor,factor2)
+
+def factor_prime_tuple(nr):
+    facs=factor_prime(nr)
+    factors={}
+    for fac in facs:
+        if fac in factors:
+            factors[fac]+=1
+        else:
+            factors[fac]=1
+    tupleFactors=[]
+    for k,v in factors.items():
+        tupleFactors.append((k,v))
+    return tupleFactors #[(factor, exponent),(factor2,exponent)]
 
 def factor(n):    
     return set(reduce(list.__add__, 
