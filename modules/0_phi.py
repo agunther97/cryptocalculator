6fa28e80dc4374e6c5569ba0d258b6b82b62373e
Grey Ruessler <greyruessler@gmail.com>
2019-05-03 19:56:12 -0500

    phi function

---
+++ modules/0_phi.py
@@ -0,0 +1,20 @@
+def phi(n,verbose=False):
+    primes=factor_prime_tuple(n)
+    total=1
+    if verbose:
+        print("Prime factorization:")
+        s=""
+        for tup in primes:
+            s+=str(tup[0])+superscript(tup[1])+"*"
+        s=s[:-1]
+        print(s)
+    s=""
+    for tup in primes:
+        p=tup[0]
+        e=tup[1]
+        total=total*( pow(p,e) - pow(p,e-1) )
+        s+="("+str(p)+superscript(e)+"-"+str(p)+superscript(e-1)+")*"
+    s=s[:-1]
+    if verbose:
+        print(totient+"="+s)
+    return total
