37bd281b65167bfba1314911dcec8bfb2512f654
Grey Ruessler <greyruessler@gmail.com>
2019-05-03 20:39:27 -0500

    better LUT function

--- modules/0_atable.py
+++ modules/0_atable.py
@@ -1,4 +1,10 @@
 def LUT(ar,val):
+    if type(ar)==list:
+        arLen=len(ar)
+        for i in range(arLen):
+            if ar[i]==val:
+                return i
+        return
     for k,v in ar.items():
         if v==val:
             return k
