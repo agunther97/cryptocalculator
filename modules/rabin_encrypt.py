a7d884aff4e1bc3cb9f27671a876f25fa51cf99e
Grey Ruessler <greyruessler@gmail.com>
2019-05-07 09:40:43 -0500

    tweaks

--- modules/rabin_encrypt.py
+++ modules/rabin_encrypt.py
@@ -4,8 +4,11 @@ def rabin_encrypt(x,p,q,verbose=False):
     elif q % 4 != 3:
         return 'q must = 3 in mod 4'
     n = p*q
+    if verbose:
+        print("n=p*q="+str(p)+"*"+str(q))
     y = pow(x,2,n)
     if verbose:
+        print("y = x^2 mod n = " +str(x)+superscript(2)+" mod " + str(n))
         print('y = ' + str(y))
     return y
 
