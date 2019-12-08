0c403c2418d0c29147f465f9919db596240258a3
Grey Ruessler <greyruessler@gmail.com>
2019-12-08 14:47:49 -0600

    unblocking back into hex

--- modules/1_rsa_blocks.py
+++ modules/1_rsa_blocks.py
@@ -3,7 +3,15 @@ import math
 def rsa_block(n,data):
     blockSize = math.floor(math.log(n,2))
     binaryStr = ("{0:b}".format(data))
-    binaryStr.rjust(math.ceil(len(binaryStr)/blockSize)*blockSize)
+    binaryStr.rjust(math.ceil(len(binaryStr)/blockSize)*blockSize,"0")
     parts = [ int(binaryStr[i:i+blockSize],2) for i in range(0, len(binaryStr), blockSize)]
     
     return parts
+
+def rsa_unblock(n,data):
+    blockSize = math.floor(math.log(n,2))
+    binaryStr=""
+    for x in data:
+        binaryStr +=("{0:b}".format(x)).rjust(blockSize,"0")
+    num = int(binaryStr,2)
+    return num
